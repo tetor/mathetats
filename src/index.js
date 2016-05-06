aafd01ba111c478bfa400289ebca86c743703d6a
tetor <tetor.takashi.etou@gmail.com>
2016-05-06 18:06:14 +0900

    Add median method

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ let math = require('mathjs');
 let mathetats = {
   mean: (arr) => {
     return math.mean(arr);
-  }
+  },
+  median: (arr) => {
+    return math.median(arr);
+  },
 };
+
 
 module.exports = mathetats;
 // export default mathetats;
--- test/mathetats.spec.js
+++ test/mathetats.spec.js
@@ -17,4 +17,18 @@ describe('mathetats', () => {
       mts.mean([1, 2]).should.equal(1.5);
     });
   });
+
+  describe('#median', () => {
+    it('returns Number', () => {
+      mts.median([1, 2, 3]).should.be.a('number');
+    });
+
+    it('returns median', () => {
+      mts.median([1, 2, 3]).should.equal(2);
+    });
+
+    it('returns avarage of two middle values if even values are passed', () => {
+      mts.median([1, 2, 3, 4]).should.equal(2.5);
+    });
+  });
 });
