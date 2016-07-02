33b97f7af9dd7aa3dbb81e23665df441eb94152f
tetor <tetor.takashi.etou@gmail.com>
2016-07-03 02:56:41 +0900

    add standard diviation method

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@ const mathetats = {
   min: (arr) => math.min(arr),
   max: (arr) => math.max(arr),
   variance: (arr, normalization = 'unbiased') => math.var(arr, normalization),
+  stdDev: (arr) => math.std(arr),
 };
 
 module.exports = mathetats;
