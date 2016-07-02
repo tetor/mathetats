31e73747f06a49c20bf5085b878fc49a80e5e62a
tetor <tetor.takashi.etou@gmail.com>
2016-07-03 02:56:26 +0900

    add max method

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ const math = require('mathjs');
 const mathetats = {
   mean: (arr) => math.mean(arr),
   median: (arr) => math.median(arr),
-  variance: (arr, normalization = 'unbiased') => math.var(arr, normalization),
   min: (arr) => math.min(arr),
+  max: (arr) => math.max(arr),
+  variance: (arr, normalization = 'unbiased') => math.var(arr, normalization),
 };
 
 module.exports = mathetats;
